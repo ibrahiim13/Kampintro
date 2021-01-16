4dd31cd2e17e8eaab2f434c5f3776cea2bda3fe2
ibrahim çakmak <theibrahimc.13@gmail.com>
2021-01-16 23:46:34 +0300

    16 ocak Ders sonu

---
+++ Classİntro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Classİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "İbrahim";
+            int yas = 20;
+            
+
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "İbrahim";
+            kurs1.IzlenmeOranı = 68;
+
+            Console.WriteLine(kurs1.KursAdi + " " + kurs1.Egitmen);
+
+
+            
+
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public string Egitmen  { get; set; }
+
+        public string IzlenmeOranı { get; set; }
+    }
+}
